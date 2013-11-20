83281877d811b7c2ab9a0c588ed71e0eb357b1ce
Kiddikn <kiddikn@gmail.com>
2013-11-20 15:55:53 +0900

    sortをfitnessで行えるようにした

--- MyfitnessComp.java
+++ MyfitnessComp.java
@@ -5,9 +5,9 @@ import java.util.*;
 class MyfitnessComp implements Comparator<Solution> {
 
 	public int compare( Solution a, Solution b ) {
-		if(a.fitnessScaling - b.fitnessScaling > 0){
+		if(a.fitness - b.fitness > 0){
 			return 1;}
-		else if(a.fitnessScaling - b.fitnessScaling < 0 ){
+		else if(a.fitness - b.fitness < 0 ){
 			return -1;}
 		else{return 0;}
 	}
